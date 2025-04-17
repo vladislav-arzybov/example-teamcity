90f07e07d89d7feab1ffe93551454bb7c8654d35
vladislav-arzybov <reivol@mail.ru>
2025-04-17 20:54:44 +0300

    hunter

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,4 +30,8 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
+	public void welcomerSaysHelp(){
+		assertThat(welcomer.sayHelp(), containsString("hunter"));
+	}
 }
